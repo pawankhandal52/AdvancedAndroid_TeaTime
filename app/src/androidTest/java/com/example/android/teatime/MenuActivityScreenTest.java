00c855fd0e2a06d92018a230363b7234429bc185
Pawan <pksharama@stemdot.com>
2018-11-01 16:38:52 +0530

    Implement Test on a Addmenu activity

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -23,6 +23,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -44,7 +53,10 @@ public class MenuActivityScreenTest {
     @Test
     public void clickGridViewItem_OpensOrderActivity() {
         //1
-        onData(allOf)
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+        
+        //2 now check tea name is same on previois grid view
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText("Green Tea")));
     }
 
 }
